0cbf6f443e4012b7ce152e2302ed4d75bd379c6e
Pr0C33D <77962630+Pr0C33D@users.noreply.github.com>
2021-10-18 19:53:28 +0300

    promises

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello')
+
+
+async function buhalteris(money1, money2) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(money1 + money2);
+        }, 1000);
+    });
+}
+
+(async () => {
+    console.log('start');
+
+    const ats1 = await buhalteris(20, 20);
+    console.log(ats1);
+
+    const ats2 = await buhalteris(ats1, 50);
+    console.log(ats2);
+    
+    console.log('done');
+})()
